531f624280a14c86cf66767a873da44667d9843f
m-kozlowski <10508687+m-kozlowski@users.noreply.github.com>
2026-03-03 10:23:23 +0100

    Modify OpenOCD command to accept configuration file
    Add conditional configuration for OpenOCD based on input argument.

--- run-ocd.sh
+++ run-ocd.sh
@@ -5,4 +5,10 @@ if [[ $(uname -r) == *microsoft-standard-WSL2* ]]; then
     lsusb | grep -q -P 'ST-?Link' || ./usbattach.sh
 fi
 
-openocd  -f interface/stlink.cfg   -f 'tcl/airsense.cfg'
+if [ -z "$1" ] ; then
+    cfg="airsense.cfg"
+else
+    cfg="${1}.cfg"
+fi
+
+openocd -f interface/stlink.cfg -f "tcl/${cfg}"
